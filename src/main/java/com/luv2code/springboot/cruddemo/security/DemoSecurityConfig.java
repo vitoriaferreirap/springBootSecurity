64bbfc3caf7e6a3535eb3a84c706f3e17b21b1d9
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-06-27 12:17:19 -0300

    Restringir o acesso com base em funções

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // spring reconhece que essa classe é uma configuração
 public class DemoSecurityConfig {
@@ -34,11 +38,31 @@ public class DemoSecurityConfig {
         UserDetails susan = User.builder()
                 .username("susan")
                 .password("{noop}test123")
-                .roles("EMPLOYEE", "ADMIN") // atribui as roles "EMPLOYEE" e "ADMIN"
+                .roles("EMPLOYEE", "MANAGER", "ADMIN") // atribui as roles "EMPLOYEE", "MANAGER" e "ADMIN"
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan); // retorna o gerenciador de usuários com o usuário
                                                                   // criado
+    }
+
+    // Restingir o acesso com base em funçoes
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE") // acesso permitido apenas para
+                                                                                       // usuario com role EMPLOYEE
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+
+        // USE HTTP BASIC AUTHENTICATION
+        http.httpBasic(Customizer.withDefaults());
+
+        // desabilita a proteçao CSRF
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 
 }
