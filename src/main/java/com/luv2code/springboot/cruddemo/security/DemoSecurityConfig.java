6605b97d4ff9223978db81e7734174ebf7aa02be
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-06-27 13:07:12 -0300

    feacture: Usando autenticação JDBC / Criação de tabelas e relacionamento MySql (usurs e authorities)

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,48 +1,27 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // spring reconhece que essa classe é uma configuração
 public class DemoSecurityConfig {
 
-    // autenticaçao da memoria
     /*
-     * informar ao Spring que o método anotado retorna um objeto que deve ser
-     * gerenciado pelo contêiner de injeção de dependência do Spring — ou seja, ele
-     * vira um bean.
+     * Usando autenticação JDBC com nossa base de dados
+     * Leitura dos usuario e as funções diretamente do banco de dados.
      */
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        // cria um usuário com nome de usuário "john" e senha "test123"
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE") // atribui a role "EMPLOYEE"
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER") // atribui as roles "EMPLOYEE" e "ADMIN"
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN") // atribui as roles "EMPLOYEE", "MANAGER" e "ADMIN"
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan); // retorna o gerenciador de usuários com o usuário
-                                                                  // criado
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     // Restingir o acesso com base em funçoes
